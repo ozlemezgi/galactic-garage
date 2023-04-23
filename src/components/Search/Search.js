05fdbaa26bcf752d13adc770fc5d532a68b487d3
ozlemezgi <ozlemezgisari@icloud.com>
2023-04-23 23:49:18 +0300

    search component edited

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import {FaSearch} from "react-icons/fa"
+import { useNavigate } from 'react-router-dom'
+import "./Search.css";
+
 
 function Search() {
   return (
-    <div>Search</div>
-  )
+    <div className="search-form">
+      <div className="container">
+        <div className="search-form-content">
+          <form className="search-form">
+            <div className="search-form-elem flex flex-sb bg-white">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Explore the vehicles... "
+              />
+              <button className="flex flex-c" type="submit">
+                <FaSearch className="text-blue" size={32} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Search
